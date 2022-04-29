5b4b8d2a9c7b3833d741f30588b2eb584ef41e50
Swati Zarekar <szarekar@upperlakeps.com>
2022-04-28 22:48:59 -0400

    👌 IMPROVE: Complete Guess game

--- index.js
+++ index.js
@@ -1,43 +1,89 @@
 let computerGuess;
 let userGuess = [];
-let userGuessUpdate = document.getElementById("textOutput");
+let userOutputGuess = document.getElementById("textOutput");
 let userNumberUpdate = document.getElementById("inputBox");
 
 const init = () => {
-    computerGuess = Math.floor(Math.random() * 100);
-    //console.log(computerGuess);
+    computerGuess = Math.floor(Math.random() * 100 + 1);
+    console.log(computerGuess);
     document.getElementById("newGameButton").style.display = "none";
     document.getElementById("gameArea").style.display = "none";
 };
+
 const startGame = () => {
     document.getElementById("welcomeScreen").style.display = "none";
     document.getElementById("gameArea").style.display = "block";
-}
-//main logic of our app
-const compareGuess = () => {
-    const userNumber = Number(document.getElementById("inputBox").value);
-    userGuess = [...userGuess, userNumber]
-    document.getElementById("guesses").innerHTML = userGuess;
-    // Check the value low or high 
-    if (userGuess > computerGuess) {
-        userGuessUpdate.innerHTML = "Your Guess is HIGH 😄 ";
-        userNumberUpdate.value = ""
-    } else if (userGuess < computerGuess) {
-        userGuessUpdate.innerHTML = "Your Guess is LOW 🙃";
-        userNumberUpdate.value = ""
-    } else {
-        userGuessUpdate.innerHTML = "It's Correct 🥳";
-        userNumberUpdate.value = ""
-    }
-    document.getElementById("attempts").innerHTML = userGuess.length
+};
 
-}
+/**
+ * reload the page when user click on newgame button
+ */
+
+const newGameBegin = () => {
+    window.location.reload();
+};
+
+/**
+ * button to create a new game & make input field disabled
+ */
+
+const startNewGame = () => {
+    document.getElementById("newGameButton").style.display = "inline";
+    userNumberUpdate.setAttribute("disabled", true);
+};
+
+/**
+ * main logic of our app
+ */
+const compareGuess = () => {
+    let userNumber = Number(document.getElementById("inputBox").value);
+    userGuess = [...userGuess, userNumber];
+    document.getElementById("guesses").innerHTML = userGuess;
+    console.log(userGuess);
+
+    /**
+     * check if the guessNumber is low hight or correct
+     */
+    if (userGuess.length < maxGuess) {
+        if (userNumber > computerGuess) {
+            userOutputGuess.innerHTML = "Your guess is High ⬆️";
+            userNumberUpdate.value = "";
+        } else if (userNumber < computerGuess) {
+            userOutputGuess.innerHTML = "Your guess is Low 👎";
+            userNumberUpdate.value = "";
+        } else {
+            userOutputGuess.innerHTML = "It's Correct😀";
+            userNumberUpdate.value = "";
+            startNewGame();
+        }
+    } else {
+        if (userNumber > computerGuess) {
+            userOutputGuess.innerHTML = `You Loose!!🥴 Correct number was ${computerGuess}`;
+            userNumberUpdate.value = "";
+            startNewGame();
+        } else if (userNumber < computerGuess) {
+            userOutputGuess.innerHTML = `You Loose!!😧 Correct number was ${computerGuess}`;
+            userNumberUpdate.value = "";
+            startNewGame();
+        } else {
+            userOutputGuess.innerHTML = "It's Correct 🤩";
+            userNumberUpdate.value = "";
+            startNewGame();
+        }
+    }
+
+    /**
+     * show the previous attempts number
+     */
+    document.getElementById("attempts").innerHTML = userGuess.length;
+};
+
 const easyMode = () => {
     maxGuess = 10;
     startGame();
-}
+};
 
 const hardMode = () => {
     maxGuess = 5;
     startGame();
-}
+};
